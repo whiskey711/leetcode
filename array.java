0c0ce251833ed48b0c668f4ea652df4da287039f
James <linfengx@uvic.ca>
2022-06-23 17:40:09 -0700

    min subarray length

--- array.java
+++ array.java
@@ -2,9 +2,41 @@ import java.util.Arrays;
 
 public class array{
     public static void main(String[] args){
-        int[] nums = new int[]{3,2,2,3};
-        System.out.println(removeElement(nums, 2));
-        System.out.println(Arrays.toString(nums));
+        int[] nums = new int[]{1,2,3,4,5};
+        // System.out.println(removeElement(nums, 2));
+        System.out.println(minSubArrLen(nums, 11));
+    }
+    static int minSubArrLen(int[] nums, int target){
+        int minLen = nums.length+1;
+        int sum = 0;
+        int left = 0;
+        for (int right=0; right<nums.length; right++){
+            sum += nums[right];
+            while (sum >= target){
+                minLen = Math.min(minLen, right-left+1);
+                sum -= nums[left];
+                left ++;
+            }
+        }
+        if (minLen == nums.length+1) return -1;
+        else return minLen;
+    }
+    static int[] sortSquare(int[] nums){
+        int[] ans = new int[nums.length];
+        int left = 0;
+        int right = nums.length-1;
+        for (int i=nums.length-1; i>-1; i--){
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+            if (leftSq > rightSq){
+                ans[i] = leftSq;
+                left++;
+            }else{
+                ans[i] = rightSq;
+                right--;
+            }
+        }
+        return ans;
     }
     static int removeElement(int[] nums, int val){
         int left = 0;
